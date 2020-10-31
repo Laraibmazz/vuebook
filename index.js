2c363772066beba97c659b4ca84d4dcdf014f5ae
Rajab <rajabm259@gmail.com>
2020-10-31 08:33:19 -0700

    node vue template

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ var express = require('express')
 var app = express()
 var PORT = process.env.PORT || 3000
 
-app.use('/',
+app.use('/index',
     express.static('dist')
 )
+app.use('/', (req, res) => {
+    res.send('Hello world')
+})
 
 app.listen(PORT, () => {
     console.log(`Server started on ${PORT}`);
