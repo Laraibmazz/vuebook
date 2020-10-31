59c45215620b49b7e0bdb0d98417709b93d1a027
Rajab <rajabm259@gmail.com>
2020-10-31 08:54:01 -0700

    node vue template

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
