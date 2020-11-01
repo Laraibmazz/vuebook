88fb44c6b6f3233eb029359069e5a3317fa36909
Rajab <rajabm259@gmail.com>
2020-11-01 02:37:04 -0800

    Json Server setup

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express')
 const app = express()
 var port = process.env.PORT || 3000
 
-app.use('/:channel/:vid', (req, res) => {
-    res.send(`HEllo`)
-})
+
 
 app.listen(port)
