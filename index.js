8a09ac81dbb70352debedad905b5b7b4a36c5744
Rajab <rajabm259@gmail.com>
2020-10-31 23:09:01 -0700

    ssetup

--- index.js
+++ index.js
@@ -6,4 +6,16 @@ app.use('/',
     express.static('dist')
 )
 
+app.use('/:channel', (req, res) => {
+    res.send(`This is channel: ${req.params.channel}`)
+})
+
+app.use('/',
+    express.static('dist')
+)
+
+app.use('/',
+    express.static('dist')
+)
+
 app.listen(port)
