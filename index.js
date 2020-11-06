5b6c2f4827e89e8b7cbcb4a790d3560f80c2b3ee
Rajab <rajabm259@gmail.com>
2020-11-06 07:53:29 -0800

    Created Home.vue component

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const express = require('express');
 const app = express()
 const PORT = process.env.PORT || 3000
 
-var router = require('./router')
-
-app.use('/index', router)
+app.use('/index', express.static('dist'))
 
 
 
 app.listen(PORT, () => {
     console.log(`Server started on ${PORT}`);
 });
+
---
+++ src/components/button.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <button type="button" class="btn btn-light btn-block mt-2 rounded-0">
+      More
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "button",
+};
+</script>
