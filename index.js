9197a355c4bec72600ad055850000b33fe353c92
Rajab <rajabm259@gmail.com>
2020-11-07 04:59:39 -0800

    home.vue component

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const PORT = process.env.PORT || 3000
 
 app.use('/index', express.static('dist'))
 
+
+app.use('/css', express.static('dist/css'))
+
+
+app.use('/js', express.static('dist/js'))
+
+
 app.listen(PORT, () => {
     console.log(`Server started on ${PORT}`);
 });
